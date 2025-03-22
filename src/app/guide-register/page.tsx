3d7a4f2baa9ea003f5d8e539bb0636a43d874445
Saurav Karki <sauravkarki10.12@gmail.com>
2025-03-22 18:06:03 +0545

    GMN-8 created register for guide

---
+++ src/app/guide-register/page.tsx
@@ -0,0 +1,12 @@
+import GuideRegister from "@/components/Login/GuideRegister"
+import React from "react"
+
+const page = () => {
+  return (
+    <div>
+      <GuideRegister />
+    </div>
+  )
+}
+
+export default page
