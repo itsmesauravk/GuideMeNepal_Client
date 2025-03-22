00cd93945ce3e3956fb5422d20af050808988672
Saurav Karki <sauravkarki10.12@gmail.com>
2025-03-22 17:56:44 +0545

    GMN-3 created login for user with email and oauth

---
+++ src/app/login/guide/page.tsx
@@ -0,0 +1,12 @@
+import GuideLoginForm from "@/components/Login/LoginGuide"
+import React from "react"
+
+const page = () => {
+  return (
+    <div className="w-full">
+      <GuideLoginForm />
+    </div>
+  )
+}
+
+export default page
