5411aee6958556ede2919e02e49e5bed0f7b4454
Saurav Karki <sauravkarki10.12@gmail.com>
2025-03-22 18:03:24 +0545

    GMN-8 created register for users

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterFormClient from "@/components/Login/RegisterForm"
+import React from "react"
+
+const page = () => {
+  return (
+    <div>
+      <RegisterFormClient />
+    </div>
+  )
+}
+
+export default page
