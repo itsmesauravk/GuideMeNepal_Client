e49d56fee1381cf77f49bddb549998b35a94989a
Saurav Karki <sauravkarki10.12@gmail.com>
2025-04-22 15:38:26 +0545

    GMN-16 modified: added suspense for loading

--- src/app/guide/messages/page.tsx
+++ src/app/guide/messages/page.tsx
@@ -1,15 +1,23 @@
 import Sidebar from "@/components/common/Sidebar"
 import UserMessages from "@/components/Messages/UserMessage"
+import { Loader2Icon } from "lucide-react"
 
-import React from "react"
+import React, { Suspense } from "react"
 
 const page = () => {
   return (
     <div className="flex">
       {/* Sidebar */}
       <Sidebar />
-
-      <UserMessages />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center w-full h-screen">
+            <Loader2Icon className="animate-spin" />
+          </div>
+        }
+      >
+        <UserMessages />
+      </Suspense>
     </div>
   )
 }
